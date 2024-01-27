1617591a01a0b5afb8081b4876d8a393a788907c
ushka1 <david.prajzner01@gmail.com>
2024-01-27 13:11:26 +0100

    add more tests

--- UniquelyDecodableTest.hs
+++ UniquelyDecodableTest.hs
@@ -2,16 +2,27 @@ module UniquelyDecodableTest where
 
 import UniquelyDecodable (uniquelyDecodable)
 
-main :: IO ()
-main = do
+runTests :: IO ()
+runTests = do
   let testCases =
-        [ (["0", "01", "11"], True),
-          (["0", "01", "10"], False),
+        [ -- uniquely decodable
+          (["0", "1"], True),
+          (["0", "01", "11"], True),
           (["0", "10", "110", "111"], True),
+          (["0", "01", "011", "1110"], True),
+          (["0", "10", "110", "1110", "11110"], True),
+          (["0101", "1001", "10", "000", "11", "100"], True),
+          (["car", "pet", "carpets"], True),
+          (["cars", "pet", "carpet"], True),
+          -- not uniquely decodable
+          (["0", "00"], False),
+          (["0", "01", "10"], False),
           (["0", "10", "010", "101"], False),
-          (["0", "2", "10", "20", "11"], False),
+          (["0", "001", "010", "100"], False),
           (["02", "12", "120", "20", "21"], False),
-          (["car", "pet", "carpet"], False)
+          (["1", "011", "01110", "1110", "10011"], False),
+          (["car", "pet", "carpet"], False),
+          (["car", "pets", "carpets"], False)
         ]
   mapM_ runTest testCases
   where
